cd7839fd38de52d0c81d17e479b62633fd20ad62
gonzalezben81 <gonzalezben393@gmail.com>
2025-11-18 01:53:49 +0000

    Try bash again

--- aws-info-check.sh
+++ aws-info-check.sh
@@ -12,17 +12,18 @@ while IFS= read -r line
 do
   ou_id=$(echo "$line" | cut -d':' -f3)
   ou_name=$(echo "$line" | cut -d':' -f1)
-  accounts=$(aws organizations list-accounts-for-parent --parent-id ${ou_id} --output text --query 'Accounts[*].[Id,Name]')
+  accounts=$(aws organizations list-accounts-for-parent --parent-id ${ou_id} --output text --query 'Accounts[*].[Id,Name]' --profile AdministratorAccess-098789448788)
   if [[ -z "$accounts" ]]; then
     echo "No accounts found in OU \"${ou_name}\""
     continue
   fi
+  echo "Org $ou_name"
   echo ""
-  echo "Here is the list of AWS accounts in the OU \"${ou_name}\""
-  echo "--------------------------------------------------------------------"
-  echo "| Account Name                                   | Account ID      | "
-  echo "|------------------------------------------------|-----------------|"
-  echo "$accounts" | awk -F'\t' '{printf "| %-46s | %-15s |\n", $2, $1}'
-  echo "--------------------------------------------------------------------"
+  echo "Here is the list of AWS accounts in the OU $ou_id \"${ou_name}\""
+  echo "-----------------------------------------------------------------------------------"
+  echo "| Account Name                                   | Account ID      | OU Name|OU ID|"
+  echo "|------------------------------------------------|-----------------|--------|-----|"
+  echo "$accounts" | awk -F'\t' -v ou_name=$ou_name -v ou_id=$ou_id '{printf "| %-46s | %-15s | %-15s |%-15s|\n", $2, $1, ou_name, ou_id}'
+  echo "----------------------------------------------------------------------------------------------------"
   echo ""
 done < "$input_file"
